fd1c316a7a72e7c1b42379eda73c21f86f487ed0
Raff4enskii25 <evgennn901@mail.ru>
2026-09-18 21:20:58 +0300

    Add duplicate check for exchange rates

--- CbrRatesTracker/CbrRatesTracker/Services/ExchangeRateUpdateService.cs
+++ CbrRatesTracker/CbrRatesTracker/Services/ExchangeRateUpdateService.cs
@@ -21,18 +21,28 @@ namespace CbrRatesTracker.Services
         public async Task UpdateRatesAsync()
         {
             var latestRates = await _cbrClientService.GetLatestRatesAsync();
+            if(latestRates.Count == 0)
+            {
+                _logger.LogWarning("No rates received from CBR. Skipping update.");
+                return;
+            }
+
             var currencies = await _dbContext.Currencies.ToDictionaryAsync(c => c.CharCode, c => c);
+            var existingRates = _dbContext.ExchangeRates
+                .Where(r => r.Date == latestRates[0].Date)
+                .Select(r => r.Currency.CharCode)
+                .ToHashSet();
 
             foreach (var latestRate in latestRates)
             {
                 if (currencies.TryGetValue(latestRate.CharCode, out var currency))
-                    AddExchangeRate(latestRate, currency);
+                    AddExchangeRate(latestRate, currency, existingRates);
                 else
                 {
                     var newCurrency = new Currency(latestRate.CharCode, latestRate.NumCode, latestRate.Name);
                     _dbContext.Currencies.Add(newCurrency);
                     _logger.LogInformation($"New currency added: {latestRate.CharCode} - {latestRate.Name}");
-                    AddExchangeRate(latestRate, newCurrency);
+                    AddExchangeRate(latestRate, newCurrency, existingRates);
                 }
             }
 
@@ -40,8 +50,15 @@ namespace CbrRatesTracker.Services
             _logger.LogInformation($"Exchange rates update completed on {DateTime.Now}.");
         }
 
-        private void AddExchangeRate(ValuteDTO latestRate, Currency currency)
-        {
+
+        private void AddExchangeRate(ValuteDTO latestRate, Currency currency, HashSet<string> existingRates)
+        {   
+            if(existingRates.Contains(currency.CharCode))
+            {
+                _logger.LogInformation($"Exchange rate for {currency.CharCode} on {latestRate.Date} already exists. Skipping.");
+                return;
+            }
+            
             var newRate = new ExchangeRate(currency, latestRate.Date, latestRate.Nominal, latestRate.Value);
             _dbContext.Add(newRate);
             _logger.LogInformation($"New exchange rate added for {currency.CharCode} on {latestRate.Date}: {latestRate.Value}");
